3f589c333c004454f72b2a800297fb69c0fcba1e
Mhutashim <billamdmhutashim@gmail.com>
2023-11-30 16:15:44 +0600

    TodoInput UI done

--- src/App.js
+++ src/App.js
@@ -1,54 +1,98 @@
 // import logo from './logo.svg';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {icon} from '@fortawesome/fontawesome-svg-core';
-import TodoInput from './Components/TodoInput';
-import TodoList from './Components/TodoList';
-import uuid from 'uuid'; // added npm modules
-import 'bootstrap/dist/css/bootstrap.min.css'; //Added in the npm modules
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import TodoInput from "./Components/TodoInput";
+import TodoList from "./Components/TodoList";
+import { v4 as uuid } from "uuid"; // added npm modules
+import "bootstrap/dist/css/bootstrap.min.css"; //Added in the npm modules
 
-
-//test 
-import { faAtom } from '@fortawesome/free-solid-svg-icons'; //✅ individual import style
-import {faBicycle} from '@fortawesome/free-solid-svg-icons';
-  // got it from https://stackoverflow.com/questions/42522233/react-fontawesome-not-displaying-icons 
+//test
+import { faAtom } from "@fortawesome/free-solid-svg-icons"; //✅ individual import style
+import { faBicycle } from "@fortawesome/free-solid-svg-icons";
+// got it from https://stackoverflow.com/questions/42522233/react-fontawesome-not-displaying-icons
 
 //functional component
 const TestFontAwesome = () => {
-  const element = <FontAwesomeIcon icon={faAtom} />
-  return(
+  const element = <FontAwesomeIcon icon={faAtom} />;
+  return (
     <>
       <h4>FontAwesome testing component</h4>
-      <FontAwesomeIcon icon={icon({name: 'user-secret'})}  ></FontAwesomeIcon>  {/* dynamic import style */}
-      <FontAwesomeIcon icon={icon({name: 'twitter', style: 'brands'})} /> 
-      <span className='btn-color'>
-        <FontAwesomeIcon icon={icon({name:'rectangle-xmark', style:'regular' })} />
+      <FontAwesomeIcon
+        icon={icon({ name: "user-secret" })}
+      ></FontAwesomeIcon>{" "}
+      {/* dynamic import style */}
+      <FontAwesomeIcon icon={icon({ name: "twitter", style: "brands" })} />
+      <span className="btn-color">
+        <FontAwesomeIcon
+          icon={icon({ name: "rectangle-xmark", style: "regular" })}
+        />
       </span>
-        <p>{element}</p> {/* ✅ */}
-        <FontAwesomeIcon icon={faBicycle} beat /> {/* ✅ */}
+      <p>{element}</p> {/* ✅ */}
+      <FontAwesomeIcon icon={faBicycle} beat /> {/* ✅ */}
       <h4>End FontAwesome testing component</h4>
     </>
   );
-}
+};
 
-function App() {
+export default class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "Brush teeth" },
+      { id: 3, title: "Eat BreakFast" },
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  };
 
-  return (
-    <div >
-      <div className="container">
-        <div className="row justify-content-center">
-          {/* <div className="col-4 bg-warning">
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle submit");
+  };
+  clearList = () => {
+    console.log("clear list");
+  };
+  handleDelete = (id) => {
+    console.log(`delete item ${id}`);
+  };
+  handleEdit = (id) => {
+    console.log(`edit item ${id}`);
+  };
+
+  render() {
+    // console.log(this.state);
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            {/* <div className="col-4 bg-warning">
             hello world!
           </div>✅*/}
-          {/* <div className="col-6 justify-content-center align-items-center"> */}
-          <div className="col-8 bg-primary text-center  ">
-            <TodoInput></TodoInput>
-            <TodoList></TodoList>
+            {/* <div className="col-6 justify-content-center align-items-center"> */}
+
+            <div className="col-10 col-md-8 mx-auto mt-5 ">
+              <h3 className="text-center text-capitalize">Todo Input</h3>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              ></TodoInput>
+              <h3 className="text-center text-capitalize">Todo List</h3>
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleEdit={this.handleEdit}
+                handleDelete={this.handleDelete}
+              ></TodoList>
+            </div>
           </div>
-          
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
 
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
-      <div>
-        <h3>from todo input</h3>
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-text bg-primary text-white">
+              <FontAwesomeIcon icon={faBook} />
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="d-grid gap-2">
+            <button
+              type="submit"
+              className="btn btn-primary mt-3 text-uppercase"
+            >
+              add item
+            </button>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
