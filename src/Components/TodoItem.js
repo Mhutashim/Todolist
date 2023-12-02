cec627edd7f2fa0cca28d19e5d6321e6437508a9
Mhutashim <billamdmhutashim@gmail.com>
2023-12-02 10:53:15 +0600

    TodoItem done UI,Logic

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default class TodoItem extends Component {
   render() {
     // console.log(this.props);
     const { title, handleDelete, handleEdit } = this.props;
-    return <div>{title}</div>;
+    return (
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <h6>{title}</h6>
+        <div className="todo-icon">
+          <span className="text-success mx-2" onClick={handleEdit}>
+            <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
+          </span>
+          <span className="text-danger mx-2" onClick={handleDelete}>
+            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+          </span>
+        </div>
+      </li>
+    );
   }
 }
